fc999090b2c688aa0645a9822bb87d9fe31fe5ad
mi2428 <mi2428782020@gmail.com>
2019-04-29 00:20:05 +0900

    migrate
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- migrations/10_deploy_cleaning_duty.js
+++ migrations/10_deploy_cleaning_duty.js
@@ -2,7 +2,5 @@ const CleaningDuty = artifacts.require("CleaningDuty");
 const num_of_cleaners = 2;
 
 module.exports = function(deployer) {
-  return deployer.then(() => {
-    return deployer.deploy(CleaningDuty, num_of_cleaners);
-  });
+  deployer.deploy(CleaningDuty, num_of_cleaners);
 }
