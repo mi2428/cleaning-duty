4d5672ae22fb693e069b8b3d2caeeaafb5725065
mi2428 <mi2428782020@gmail.com>
2019-04-29 14:26:41 +0900

    It works
    Signed-off-by: mi2428 <mi2428782020@gmail.com>

--- contracts/CleaningDuty.sol
+++ contracts/CleaningDuty.sol
@@ -1,16 +1,22 @@
 pragma solidity >=0.5.2 <0.6.0;
 
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
-import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract CleaningDuty is ERC20Mintable, ERC20Burnable, ERC20Detailed {
+
+contract CleaningDuty is ERC20Detailed, ERC20Mintable, ERC20Burnable {
   string private _name = "CleaningDuty";
   string private _symbol = "DUTY";
   uint8 private _decimals = 0;
 
   constructor(uint256 initSupply)
-    ERC20Mintable() ERC20Burnable() ERC20Detailed(_name, _symbol, _decimals) public {
+    ERC20Detailed(_name, _symbol, _decimals) ERC20Mintable() ERC20Burnable() public {
     _mint(msg.sender, initSupply);
+  }
+
+  function _transfer(address from, address to, uint256 value) internal {
+    require(balanceOf(to) + value <= 1, "Number of DUTYs assigned to a student is at most 1.");
+    super._transfer(from, to, value);
   }
 }
